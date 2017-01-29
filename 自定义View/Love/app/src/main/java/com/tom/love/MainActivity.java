9fe57257dd2ef3f93ada4ecd85fafe3e80fdc600
Tom <mengxinchn@gmail.com>
2017-01-29 21:07:02 +0800

    贝赛尔曲线

---
+++ 自定义View/Love/app/src/main/java/com/tom/love/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tom.love;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bezier3 bezier = new Bezier3(this);
+        setContentView(bezier);
+    }
+}
